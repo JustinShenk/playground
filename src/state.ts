a63e90a373f5d4900521d50e7a62fef5a9f4434c
Justin Shenk <justinshenk@flask-app-ubuntu1604.c.justin-217611.internal>
2019-01-02 17:27:25 +0000

    Change default network shape, update page meta info

--- src/state.ts
+++ src/state.ts
@@ -150,7 +150,7 @@ export class State {
   collectStats = false;
   numHiddenLayers = 1;
   hiddenLayerControls: any[] = [];
-  networkShape: number[] = [4, 6, 4, 6];
+  networkShape: number[] = [4, 4, 4, 4];
   x = true;
   y = true;
   xTimesY = false;
